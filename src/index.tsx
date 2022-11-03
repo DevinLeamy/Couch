2383cf5b8e44e63d486272c10e1547f2b132e8da
Devin Leamy <devinleamy@gmail.com>
2022-11-03 19:55:58 -0400

    created react-three/fiber project

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import ReactDOMClient, { createRoot } from 'react-dom/client'
+import * as React from 'react'
+import './main.css'
+import App from './App'
+
+ReactDOMClient.createRoot(document.getElementById('root')!).render(<App />)
